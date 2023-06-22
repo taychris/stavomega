af21b80bf8b5cd98afd9aaa59eb46b3e37972387
Christopher Szabo <tay.christopha@gmail.com>
2023-06-22 16:16:19 +0200

    added temporary gdpr

--- src/app/gdpr/page.tsx
+++ src/app/gdpr/page.tsx
@@ -1,8 +1,147 @@
 const GDPR = () => {
   return (
-    <main className="w-full h-[100dvh] flex items-center justify-center px-8">
-        <h1 className="font-medium text-2xl text-center">Čoskoro nahráme dokument o pravidiel ochrany osobných údajov.</h1>
+    <main className="w-full min-h-[100dvh] flex flex-col items-center justify-center px-8 mt-24">
+      <h1 className="w-full mb-5 text-2xl font-medium">
+        Ochrana osobných údajov.
+      </h1>
+      <div className="w-full space-y-2">
+        <p>
+          Tieto Zásady ochrany osobných údajov popisujú, ako spoločnosť
+          Stavomega s.r.o. („Spoločnosť“, „my“, „náš“ alebo „nás“) zhromažďuje,
+          používa a chráni osobné údaje poskytnuté jednotlivcami („vy“ alebo
+          „používatelia“) pri používaní našej webovej stránky. Zaviazali sme sa
+          chrániť vaše súkromie a zaistiť bezpečnosť vašich osobných údajov.
+          Pozorne si prečítajte tieto Zásady ochrany osobných údajov, aby ste
+          pochopili naše postupy týkajúce sa vašich osobných údajov.
+        </p>
+        <h2 className="font-semibold">1. Informácie, ktoré zhromažďujeme</h2>
+        <p>
+          Keď navštívite našu webovú stránku alebo použijete náš kontaktný
+          formulár, môžeme od vás získať nasledujúce osobné údaje:
+        </p>
+        <p>
+          1.1. Celé meno: Môžeme zhromažďovať vaše celé meno, aby sme
+          prispôsobili našu komunikáciu a správne vás oslovovali.
+        </p>
+        <p>
+          1.2. E-mailová adresa: Zhromažďujeme vašu e-mailovú adresu, aby sme
+          odpovedali na vaše otázky, poskytovali informácie o našich produktoch
+          alebo službách a posielali vám aktualizácie alebo propagačné
+          materiály, ak ste sa rozhodli ich dostávať.
+        </p>
+        <p>
+          1.3. Telefónne číslo: Ak uvediete svoje telefónne číslo, môžeme ho
+          použiť na to, aby sme vás kontaktovali v súvislosti s vašimi otázkami
+          alebo poskytli zákaznícku podporu.
+        </p>
+        <p>
+          1.4. Správa: Zhromažďujeme obsah správy, ktorú odošlete
+          prostredníctvom kontaktného formulára, aby sme mohli efektívne riešiť
+          vašu otázku alebo požiadavku.
+        </p>
+        <h2 className="font-semibold">2. Používanie osobných údajov</h2>
+        <p>
+          Vami poskytnuté osobné údaje používame výhradne na nasledujúce účely:
+        </p>
+        <p>
+          2.1. Komunikácia: Vašu e-mailovú adresu a telefónne číslo môžeme
+          použiť na odpovedanie na vaše otázky, poskytovanie informácií alebo
+          komunikáciu s vami ohľadom našich produktov, služieb alebo iných
+          relevantných záležitostí.
+        </p>
+        <p>
+          2.2. Zákaznícka podpora: Ak sa na nás obrátite so žiadosťou o pomoc,
+          môžeme použiť informácie, ktoré nám poskytnete, na poskytnutie podpory
+          a vyriešenie akýchkoľvek problémov, s ktorými sa môžete stretnúť.
+        </p>
+        <p>
+          2.3. Zlepšenie našich služieb: Môžeme analyzovať informácie, ktoré nám
+          poskytnete, aby sme pochopili vaše preferencie, vylepšili našu webovú
+          stránku, vyvinuli nové funkcie alebo prispôsobili naše produkty a
+          služby tak, aby lepšie vyhovovali vašim potrebám.
+        </p>
+        <h2 className="font-semibold">3. Bezpečnosť a uchovávanie údajov</h2>
+        <p>
+          Zavádzame štandardné bezpečnostné opatrenia na ochranu vašich osobných
+          údajov pred neoprávneným prístupom, zverejnením alebo zmenou.
+          Používame technológiu šifrovania Secure Socket Layer (SSL), aby sme
+          zabezpečili dôvernosť vašich údajov počas prenosu. Upozorňujeme však,
+          že žiadny spôsob prenosu cez internet alebo elektronické ukladanie nie
+          je 100% bezpečný.
+        </p>
+        <p>
+          Vaše osobné údaje uchovávame len tak dlho, ako je potrebné na splnenie
+          účelov uvedených v týchto Zásadách ochrany osobných údajov, pokiaľ
+          zákon nevyžaduje dlhšiu dobu uchovávania.
+        </p>
+        <h2 className="font-semibold">4. Zdieľanie osobných údajov</h2>
+        <p>
+          Vaše osobné údaje nepredávame, neobchodujeme, neprenajímame ani ich
+          nezverejňujeme tretím stranám, s výnimkou prípadov popísaných v týchto
+          zásadách ochrany osobných údajov alebo s vaším súhlasom. Vaše osobné
+          údaje môžeme zdieľať za nasledujúcich okolností:
+        </p>
+        <p>
+          4.1. Poskytovatelia služieb: Môžeme zapojiť dôveryhodných
+          poskytovateľov služieb tretích strán, ktorí nám pomáhajú pri prevádzke
+          našej webovej stránky alebo pri poskytovaní služieb vám. Títo
+          poskytovatelia služieb sú zmluvne zaviazaní zaobchádzať s vašimi
+          osobnými údajmi v súlade s týmito zásadami ochrany osobných údajov a
+          platnými zákonmi o ochrane údajov.
+        </p>
+        <p>
+          4.2. Právne požiadavky: Vaše osobné údaje môžeme zverejniť, ak to
+          vyžaduje zákon alebo ako odpoveď na platnú právnu žiadosť, ako je
+          súdny príkaz, vládne vyšetrovanie alebo na ochranu našich práv,
+          majetku alebo bezpečnosti.
+        </p>
+        <h2 className="font-semibold">5. Vaše práva a voľby</h2>
+        <p>
+          5.1. Prístup a aktualizácia: Máte právo na prístup k svojim osobným
+          údajom, ich opravu alebo aktualizáciu. Ak potrebujete pomoc,
+          kontaktujte nás pomocou informácií uvedených v časti „Kontaktujte nás“
+          nižšie.
+        </p>
+        <p>
+          5.2. Marketingová komunikácia: Ak si už od nás neželáte dostávať
+          marketingovú komunikáciu, môžete sa odhlásiť alebo odhlásiť podľa
+          pokynov uvedených v našich e-mailoch alebo nás môžete priamo
+          kontaktovať.
+        </p>
+        <h2 className="font-semibold">
+          6. Odkazy na webové stránky tretích strán
+        </h2>
+        <p>
+          Naša webová stránka môže obsahovať odkazy na webové stránky tretích
+          strán. Upozorňujeme, že nie sme zodpovední za postupy ochrany osobných
+          údajov, obsah alebo zásady týchto webových stránok. Odporúčame vám,
+          aby ste si prečítali zásady ochrany osobných údajov všetkých stránok
+          tretích strán, ktoré navštívite.
+        </p>
+        <h2 className="font-semibold">
+          7. Zmeny týchto zásad ochrany osobných údajov
+        </h2>
+        <p>
+          Tieto Zásady ochrany osobných údajov môžeme z času na čas
+          aktualizovať. Akékoľvek zmeny, ktoré vykonáme, budú zverejnené na
+          tejto stránke s upraveným dátumom účinnosti. Odporúčame vám pravidelne
+          kontrolovať tieto Zásady ochrany osobných údajov, aby ste zostali
+          informovaní o tom, ako zhromažďujeme, používame a chránime vaše osobné
+          údaje.
+        </p>
+        <h2 className="font-semibold">Kontaktuj nás</h2>
+        <p>
+          Ak máte akékoľvek otázky, obavy alebo požiadavky týkajúce sa týchto
+          Zásad ochrany osobných údajov alebo zaobchádzania s vašimi osobnými
+          údajmi, kontaktujte nás pomocou nasledujúcich informácií:
+        </p>
+        <div>
+          <p>Stavomega s.r.o.</p>
+          <p>Email: stavomega@gmail.com</p>
+          <p>Tel.: 055/460 71 02</p>
+        </div>
+      </div>
     </main>
-  )
-}
-export default GDPR
+  );
+};
+export default GDPR;
